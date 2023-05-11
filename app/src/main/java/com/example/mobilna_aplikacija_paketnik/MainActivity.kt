69e17a214397c0a143f900e7493cb1ff55ded6cb
BEAST <horlive3.newreal@hotmail.com>
2023-05-11 14:37:31 +0200

    Dodal da se zapre kamera potem ko skenira QR kodo, trenutno zapre aplikacijo

--- app/src/main/java/com/example/mobilna_aplikacija_paketnik/MainActivity.kt
+++ app/src/main/java/com/example/mobilna_aplikacija_paketnik/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.mobilna_aplikacija_paketnik
 
-import android.app.Activity
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
-import android.util.Size
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -13,7 +13,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Arrangement
@@ -24,11 +24,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,23 +38,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobilna_aplikacija_paketnik.ui.theme.Mobilna_aplikacija_paketnikTheme
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.app.ActivityCompat
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import com.google.zxing.ResultPoint
-import com.journeyapps.barcodescanner.BarcodeCallback
-import com.journeyapps.barcodescanner.BarcodeResult
-import com.journeyapps.barcodescanner.CompoundBarcodeView
-import java.util.jar.Manifest
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.Executors
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.DecodeHintType
+import com.google.zxing.PlanarYUVLuminanceSource
+import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.qrcode.QRCodeReader
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -172,20 +167,24 @@ fun CameraView(
             .build()
         preview.setSurfaceProvider(previewView.surfaceProvider)
         val imageAnalysis = ImageAnalysis.Builder()
-            .setTargetResolution(
-                Size(
-                    previewView.width,
-                    previewView.height
-                )
-            )
-            .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
             .build()
-        imageAnalysis.setAnalyzer(
-            ContextCompat.getMainExecutor(context),
-            QrScanner { result ->
-                onCodeScanned(result)
+            .also {
+                it.setAnalyzer(Executors.newSingleThreadExecutor(),
+                    { image ->
+                        // Process the image and extract the code
+                        val result = processImage(image)
+                        if (!result.isNullOrEmpty()) {
+                            // Display the toast
+                            Toast.makeText(context, result, Toast.LENGTH_SHORT).show()
+                            Log.println(Log.INFO, "Result: ", result)
+
+
+                            // Close the camera view
+                            onCodeScanned(result)
+                        }
+                        image.close()
+                    })
             }
-        )
         try {
             cameraProviderFuture.get().bindToLifecycle(
                 lifecyleOwner,
@@ -201,6 +200,48 @@ fun CameraView(
         modifier = Modifier
             .fillMaxSize()
     )
+}
+
+
+@SuppressLint("UnsafeOptInUsageError")
+fun processImage(image: ImageProxy): String? {
+    val mediaImage: Image? = image.image
+
+    mediaImage?.let { image ->
+        val width = image.width
+        val height = image.height
+
+        val yBuffer = image.planes[0].buffer
+        val uBuffer = image.planes[1].buffer
+        val vBuffer = image.planes[2].buffer
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val data = ByteArray(ySize + uSize + vSize)
+
+        yBuffer.get(data, 0, ySize)
+        uBuffer.get(data, ySize, uSize)
+        vBuffer.get(data, ySize + uSize, vSize)
+
+        val source = PlanarYUVLuminanceSource(data, width, height, 0, 0, width, height, false)
+
+        val binarizer = HybridBinarizer(source)
+        val binaryBitmap = BinaryBitmap(binarizer)
+
+
+        val reader = QRCodeReader()
+
+        try {
+            val result = reader.decode(binaryBitmap)
+            return result.text
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    return null
 }
 
 
