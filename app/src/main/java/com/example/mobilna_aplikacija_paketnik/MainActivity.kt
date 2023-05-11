06bff9932935a50d2616c1d8e82df16601658ace
BEAST <horlive3.newreal@hotmail.com>
2023-05-11 11:11:18 +0200

    Naredil izgled za gumb

--- app/src/main/java/com/example/mobilna_aplikacija_paketnik/MainActivity.kt
+++ app/src/main/java/com/example/mobilna_aplikacija_paketnik/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.mobilna_aplikacija_paketnik
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobilna_aplikacija_paketnik.ui.theme.Mobilna_aplikacija_paketnikTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +25,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             Mobilna_aplikacija_paketnikTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    openBoxButton {
+
+                    }
                 }
             }
         }
@@ -30,17 +40,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobilna_aplikacija_paketnikTheme {
-        Greeting("Android")
+fun openBoxButton(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = onClick,
+            shape = CircleShape,
+            modifier = Modifier.padding(16.dp).size(150.dp)
+        ) {
+            Text(text = "OPEN THE BOX")
+        }
     }
 }
