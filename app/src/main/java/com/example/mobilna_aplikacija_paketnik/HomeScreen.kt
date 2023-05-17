e21c6f84c45b472c46f4426aea5ac6a88f5ce4ab
LeonardoP01 <leonardo.puncec@student.um.si>
2023-05-17 15:34:39 +0200

    Overhaul: Implementacija NavControllerja za premik med Screens

---
+++ app/src/main/java/com/example/mobilna_aplikacija_paketnik/HomeScreen.kt
@@ -0,0 +1,43 @@
+package com.example.mobilna_aplikacija_paketnik
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Camera App") }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { navController.navigate("camera") },
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text("Scan QR Code")
+                }
+            }
+        }
+    )
+}
