14e08e3ef7fd9c76f6a9b6259f679e7dd57574d7
MIKOFI <filip.mihelic@student.um.si>
2023-05-19 22:18:58 +0200

    Spremeba MainActivity - Dodan nov retrofit builder za API.

--- app/src/main/java/com/example/mobilna_aplikacija_paketnik/MainActivity.kt
+++ app/src/main/java/com/example/mobilna_aplikacija_paketnik/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mobilna_aplikacija_paketnik.API.Login.LoginRequest
 import com.example.mobilna_aplikacija_paketnik.API.Login.loginInterface
+import com.example.mobilna_aplikacija_paketnik.OpenBox.OpenInterface
 import com.example.mobilna_aplikacija_paketnik.screens.CameraScreen
 import com.example.mobilna_aplikacija_paketnik.screens.HomeScreen
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 
 class MainActivity : ComponentActivity() {
@@ -22,21 +24,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:3001/") // Replace with your API base URL
+            .baseUrl("http://192.168.0.44:3001/") // Replace with your API base URL
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
+        val retrofitAPI = Retrofit.Builder()
+            .baseUrl("https://api-d4me-stage.direct4.me/sandbox/v1/") // Replace with your API base URL
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
         val loginInter = retrofit.create(loginInterface::class.java)
+        val openInter=retrofitAPI.create(OpenInterface::class.java)
 
         setContent {
             val navController = rememberNavController()
 
-            NavHost(navController, startDestination = "login") {
+            NavHost(navController, startDestination = "home") {
                 composable("home") {
                     HomeScreen(navController)
                 }
                 composable("camera") {
-                    CameraScreen(navController)
+                    CameraScreen(navController,openInter)
                 }
                 composable("login") {
                     LoginScreen(loginInter,navController = navController)
