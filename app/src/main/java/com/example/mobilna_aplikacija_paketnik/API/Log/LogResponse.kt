5e268dabd030f9f4e0170b1c259d04ea1f24a287
MIKOFI <filip.mihelic@student.um.si>
2023-05-25 15:18:37 +0200

    implementacija LogRequest, LogResponse

--- app/src/main/java/com/example/mobilna_aplikacija_paketnik/API/Log/LogResponse.kt
+++ app/src/main/java/com/example/mobilna_aplikacija_paketnik/API/Log/LogResponse.kt
@@ -1,4 +1,11 @@
 package com.example.mobilna_aplikacija_paketnik.API.Log
+import org.bson.types.ObjectId
+import java.util.Date
 
-class LogResponse {
-}
+data class LogResponse(
+        val user: String,
+        val opend: Date,
+        val user_id: ObjectId,
+        val boxId: Int,
+        val force: Boolean = false
+)
