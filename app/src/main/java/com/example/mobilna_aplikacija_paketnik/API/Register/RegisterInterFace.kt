eae202c8ce7daca2eddd91638a785ebefbeb89ac
leon.tikvic <leon.tikvic@gmail.com>
2023-06-03 15:13:24 +0200

    Fixed multiparting

--- app/src/main/java/com/example/mobilna_aplikacija_paketnik/API/Register/RegisterInterFace.kt
+++ app/src/main/java/com/example/mobilna_aplikacija_paketnik/API/Register/RegisterInterFace.kt
@@ -1,12 +1,23 @@
+import android.service.autofill.UserData
 import com.example.mobilna_aplikacija_paketnik.API.Register.RegisterResponse
+import com.example.mobilna_aplikacija_paketnik.API.Register.RegisterUser
 import okhttp3.MultipartBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface RegisterInterFace {
+    @POST("/users")
+    suspend fun register(
+      @Body requestBody: RegisterUser
+    ): Response<RegisterResponse>
+
     @Multipart
     @POST("/users/register")
-    suspend fun register(@Part parts: List<MultipartBody.Part>): Response<RegisterResponse>
+    suspend fun uploadImages(
+        @Part images: List<MultipartBody.Part>
+    ): Response<RegisterResponse>
 }
+
